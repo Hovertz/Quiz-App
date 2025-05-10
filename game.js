b6e78e3489a7d232237f040e39b2e49ccc8650a7
Adam Hovertz <adamko.khan@gmail.com>
2025-05-10 15:37:29 +0300

    feedback for correct/incorrect answers, score updated

--- game.js
+++ game.js
@@ -1,38 +1,107 @@
+//import { questions } from "/data.js";
+//console.log(questions);
+let questions = [
+  {
+    question: "Which team won the Champion's League in the season 2023/2024?",
+    choice1: "Atalanta",
+    choice2: "Bayer Leverkusen",
+    choice3: "Borussia Dortmund",
+    choice4: "Real Madrid",
+    answer: 4,
+  },
+  {
+    question: "Which team has won the most World Cups?",
+    choice1: "Argentina",
+    choice2: "Brazil",
+    choice3: "France",
+    choice4: "Spain",
+    answer: 2,
+  },
+  {
+    question: "When did the World Cup start?",
+    choice1: "1928",
+    choice2: "1929",
+    choice3: "1930",
+    choice4: "1931",
+    answer: 3,
+  },
+  {
+    question: "What is the only Swedish club that won the Champion's League?",
+    choice1: "Goteborg",
+    choice2: "Linkoping",
+    choice3: "Malmo",
+    choice4: "Stockholm",
+    answer: 1,
+  },
+  {
+    question: "Where is Mehdi Taremi from?",
+    choice1: "Iran",
+    choice2: "Morocco",
+    choice3: "Qatar",
+    choice4: "Saudi Arabia",
+    answer: 1,
+  },
+  {
+    question:
+      "Which player was a legend at Barcelona then moved to PSG, and lastly Inter Miami?",
+    choice1: "Busquets",
+    choice2: "Neymar",
+    choice3: "Alba",
+    choice4: "Messi",
+    answer: 4,
+  },
+  {
+    question: "How many Ballon d'Ors does Ronaldo have?",
+    choice1: "4",
+    choice2: "5",
+    choice3: "6",
+    choice4: "7",
+    answer: 2,
+  },
+  {
+    question: "Which anime is about Football?",
+    choice1: "Bleach",
+    choice2: "Solo Levelling",
+    choice3: "Blue Lock",
+    choice4: "Haikyuu",
+    answer: 3,
+  },
+  {
+    question: "Who is the only goalkeeper that has won the Ballon'Dor?",
+    choice1: "Donnarumma",
+    choice2: "Yashin",
+    choice3: "Cech",
+    choice4: "Buffon",
+    answer: 2,
+  },
+  {
+    question: "Which club went unbeaten in 2023/2024 from the Bundesliga?",
+    choice1: "Stuttgart",
+    choice2: "Bayern Munchen",
+    choice3: "Borussia Dortmund",
+    choice4: "Bayer Leverkusen",
+    answer: 4,
+  },
+  {
+    question: "Which 2 teams are in the Champions League final this season?",
+    choice1: "PSG and Barcelona ",
+    choice2: "Bayern Munchen and PSG",
+    choice3: "Inter Milan and Barcelona",
+    choice4: "PSG and Inter Milan",
+    answer: 4,
+  },
+];
+
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
-let questions = [
-  {
-    question: "Text 1",
-    choice1: "one1",
-    choice2: "two1",
-    choice3: "three1",
-    choice4: "four1",
-    answer: 1,
-  },
-  {
-    question: "Text 2",
-    choice1: "one2",
-    choice2: "two2",
-    choice3: "three2",
-    choice4: "four2",
-    answer: 1,
-  },
-  {
-    question: "Text 3",
-    choice1: "one3",
-    choice2: "two3",
-    choice3: "three3",
-    choice4: "four3",
-    answer: 1,
-  },
-];
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
@@ -51,6 +120,8 @@ getNewQuestion = () => {
     return window.location.assign("end.html");
   }
   questionCounter++;
+  questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -72,9 +143,25 @@ choices.forEach((choice) => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    console.log(selectedAnswer);
-    getNewQuestion();
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 
 startGame();
